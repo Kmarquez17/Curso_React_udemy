aa341cc1aee0c4556fe4b57d4036e93b2b4506ba
Kevin Marquez <marquezkrodriguez24@gmail.com>
2019-03-18 15:09:58 -0600

    Mostrando Noticias

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-import Header from './components/Header'
-
+import Header from './components/Header';
+import Noticias from './components/Noticias';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header titulo='Noticias' />
-      </div>
-    );
-  }
+	state = {
+		noticias: [],
+	};
+
+	componentDidMount() {
+		this.consultarNoticias();
+	}
+
+	consultarNoticias = () => {
+		let url =
+			`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4a7109eb2bc54b6a89cd9557a2803d7b`;
+
+		fetch(url)
+			.then(res => {
+				return res.json();
+			})
+			.then(noticias => {
+				this.setState({
+					noticias: noticias.articles,
+				});
+			});
+	};
+
+	render() {
+		return (
+			<div className="contenedor-app">
+				<Header titulo="Noticias" />
+				<div className="container white contenedor-noticias">
+					<Noticias noticias={this.state.noticias} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
